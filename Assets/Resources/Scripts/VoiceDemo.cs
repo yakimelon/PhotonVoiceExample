9983993cdcca8590f1575c48e2cd84951ebba9ab
Marihuana <aisatu_nomahou@yahoo.co.jp>
2018-09-12 21:19:52 +0900

    first commit

---
+++ Assets/Resources/Scripts/VoiceDemo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+
+	void OnJoinedLobby() {
+		Debug.Log("ロビーへ入室しました");
+		RoomOptions options = new RoomOptions() {
+			IsVisible = false,
+			MaxPlayers = 4
+		};
+		PhotonNetwork.JoinOrCreateRoom("room", options, TypedLobby.Default);
+	}
+
+	void OnJoinedRoom() {
+		Debug.Log("ルームへ入室しました");
+		PhotonNetwork.Instantiate(
+			"cube",	
+			Vector3.zero,
+			Quaternion.identity,
+			0
+		);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
